bad830ab5944790533367ff65760543da742e9d3
sys04176 <74278487+sys04176@users.noreply.github.com>
2021-03-11 17:47:01 +0900

    Add files via upload

---
+++ Week2/src/OneDim.java
@@ -0,0 +1,11 @@
+
+public class OneDim {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sale[]=new int[] {157,209,251,312};
+		for(int i=0;i<4;i++)
+			System.out.println(sale[i]);
+	}
+
+}
---
+++ Week2/src/TwoDim.java
@@ -0,0 +1,12 @@
+
+public class TwoDim {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sale[][]=new int[][] {{1,2,3,4},{5,6,7,8}};
+		
+		for(int i=0;i<2;i++)
+			for(int j=0;j<4;j++)
+				System.out.println(sale[i][j]);
+	}
+}
